8984b208fdc273331c0c94dbbd6335a0fc9fc4a2
Eric Lin <eric.lin@nyu.edu>
2022-05-08 22:26:15 -0400

    Updated Quiz logic and added short circuits

--- public/form.js
+++ public/form.js
@@ -7,19 +7,21 @@ let formnumber=0;
 
 next_click.forEach(function(next_page){
     next_page.addEventListener('click',function(){
-         if(!validateform()){
-        return false;
+        if(!validateform()){
+            return false;
+        } else if (shortcircuit()){
+            formnumber = 4
+            updateform();
         }
-         formnumber++;
-         updateform();
-         progress_forward();
+        formnumber++;
+        updateform();
     });
 });
 
 back_click.forEach(function(back_page){
     back_page.addEventListener('click',function(){
-         formnumber--;
-         updateform();   
+        formnumber--;
+        updateform();   
     });
 });
 
@@ -35,31 +37,69 @@ submit_click.forEach(function(submit){
             i++;
         }
         console.log(options);
-        if (options.includes('quantum-yes')) {
-            location.href = '/post-quantum';
+        if (options.includes('type-quantum')) {
+            location.href = '/gottesman';
+        } else if (options.includes('speedup-yes')) {
+            location.href = '/lamport';
+        } else if (options.includes('speed-fast')) {
+            location.href = '/ecdsa';
+        } else if (options.includes('purpose-gov')) {
+            location.href = '/dsa';
+        } else if (options.includes('purpose-easy')) {
+            location.href = '/rsa';
+        } else if (options.includes('special-threshold')) {
+            location.href = '/threshold';
+        } else if (options.includes('special-anonymous')) {
+            location.href = '/ring';
+        } else if (options.includes('special-designated')) {
+            location.href = '/designated';
+        } else {
+            location.href = '/schnorr';
         }
     });
 });
-
-function progress_forward(){
-    list[formnumber].classList.add('active');
-}
 
 function updateform(){
     main_form.forEach(function(main_number){ 
        main_number.classList.remove('active'); 
     });
     main_form[formnumber].classList.add('active');
+    list.forEach(function(list_number){ 
+        list_number.classList.remove('active'); 
+    });
+    for(var i = 0; i <= formnumber; i++){
+        list[i].classList.add('active');
+    }
 } 
- 
+
+function shortcircuit(){
+    current_checked = null;
+    short_circuit = ['type-quantum', 'speedup-yes', 'speed-fast', 'purpose-easy', 'purpose-gov'];
+    var validate_form=document.querySelectorAll(".main.active input");
+    var i = 0;
+    while (i < validate_form.length) {
+        if (validate_form[i].checked){
+            current_checked = validate_form[i].id;
+            break;
+        }
+        i++;        
+    }
+    if(short_circuit.includes(current_checked)){
+        return true;
+    }
+    return false;
+}
+
 function validateform(){
     validate=false;
+    current_checked = null;
     var validate_form=document.querySelectorAll(".main.active input");
     var i = 0;
     while (!validate && i < validate_form.length) {
         if (validate_form[i].checked){
             console.log(validate_form[i].id);
             validate = true;
+            break;
         }
         i++;        
     }
